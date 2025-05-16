7ec7dcd63c6ebff97e90a8f36e61766a077af4aa
Ali <ali.essam.abdelhaleem.20@gmail.com>
2025-05-16 08:36:01 +0300

    fix

--- lib/fetch-data.ts
+++ lib/fetch-data.ts
@@ -21,7 +21,7 @@ export async function fetchAndParseCSV(url: string): Promise<any[]> {
           })
           resolve(filteredData)
         },
-        error: (error) => {
+        error: (error: Error) => {
           reject(error)
         },
       })
