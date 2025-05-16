ff334e8008d9e961a83bf7b85346e869e2fe4ade
Ali <ali.essam.abdelhaleem.20@gmail.com>
2025-05-16 09:09:37 +0300

    update

--- app/api/csv/route.ts
+++ app/api/csv/route.ts
@@ -1,6 +1,5 @@
 export async function GET() {
-    const csvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2KXpX8_6FUj3Ks77B6WNJrw2QJlQnt0WWPSk2U6V1z8a1G5rwEnKNBqrnmT_9HZCoy5uREiZrs9uA/pub?gid=1596920951&single=true&output=csv";
-    
+    const csvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR2KXpX8_6FUj3Ks77B6WNJrw2QJlQnt0WWPSk2U6V1z8a1G5rwEnKNBqrnmT_9HZCoy5uREiZrs9uA/pub?output=csv"
     const response = await fetch(csvUrl);
     const text = await response.text();
     
